ea8cc3e8d7b9ebea9e0f369d54f00409428a11c3
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-09-22 14:30:53 +0200

    beautifed login/signup components and added remember functionality

--- gamerpilotPlatform/ClientApp/src/app/components/learningbases/learningbasis.component.ts
+++ gamerpilotPlatform/ClientApp/src/app/components/learningbases/learningbasis.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut } from '../../shared/animation';
 
 @Component({
   selector: 'app-learningbasis',
   templateUrl: './learningbasis.component.html',
-  styleUrls: ['./learningbasis.component.scss']
+  styleUrls: ['./learningbasis.component.scss'],
+  animations: [flyInOut]
+
 })
 export class LearningbasisComponent implements OnInit {
 
