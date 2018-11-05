67af5a1abbd5e164feaddecd33e7a2ef5c8d0ea3
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-05 17:12:27 +0100

    refacted FA icons into own module

--- gamerpilotPlatform/ClientApp/src/app/app.module.ts
+++ gamerpilotPlatform/ClientApp/src/app/app.module.ts
@@ -15,56 +15,9 @@ import { LearningbasisComponent } from './components/learningbases/learningbasis
 import {SnotifyModule, SnotifyService, ToastDefaults} from 'ng-snotify';
 import { TermsComponent } from './components/terms/terms.component';
 import { PrivacyPoliticsComponent } from './components/privacy-politics/privacy-politics.component';
+import { IconModule } from './modules/icon.module';
 
-/* Icons  */
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faQuoteLeft, faCodeBranch, faChalkboardTeacher, faUserCog, faChessRook,
-	faComments, faLightbulb, faPeopleCarry, faAngleDoubleRight,
-	faGlobeAmericas, faAngleDoubleDown, faChild, faDiagnoses, faBoxes,
-	faCircle, faAdjust, faFileAlt, faLongArrowAltRight, faLongArrowAltLeft,
-	faTable, faVideo, faBrain, faFileSignature, faFlagCheckered, faArrowRight,
-	faArrowLeft, faLock, faUnlock, faChevronCircleLeft, faList,
-	faDotCircle, faRocket, faTimes, faCheck, faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import { faCircle as farCircle, faPlayCircle} from '@fortawesome/free-regular-svg-icons';
 
-library.add(faAngleDown);
-library.add(faTimes);
-library.add(faCheck);
-library.add(faRocket);
-library.add(faPlayCircle);
-library.add(faDotCircle);
-library.add(faList);
-library.add(faChevronCircleLeft);
-library.add(faLock);
-library.add(faUnlock);
-library.add(faArrowRight);
-library.add(faArrowLeft);
-library.add(faTable);
-library.add(faVideo);
-library.add(faBrain);
-library.add(faFileSignature);
-library.add(faFlagCheckered);
-library.add(faQuoteLeft);
-library.add(faCodeBranch);
-library.add(faChalkboardTeacher);
-library.add(faUserCog);
-library.add(faChessRook);
-library.add(faComments);
-library.add(faLightbulb);
-library.add(faPeopleCarry);
-library.add(faAngleDoubleRight);
-library.add(faGlobeAmericas);
-library.add(faAngleDoubleDown);
-library.add(faChild);
-library.add(faDiagnoses);
-library.add(faBoxes);
-library.add(faCircle);
-library.add(faAdjust);
-library.add(farCircle);
-library.add(faFileAlt);
-library.add(faLongArrowAltRight);
-library.add(faLongArrowAltLeft);
 
 
 @NgModule({
@@ -82,7 +35,7 @@ library.add(faLongArrowAltLeft);
 			BrowserAnimationsModule,
 			SharedModule,
 			SnotifyModule,
-			FontAwesomeModule
+			IconModule
 	],
 		providers: [
 		{ provide: 'SnotifyToastConfig', useValue: ToastDefaults},
